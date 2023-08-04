773419eb413cf63b823174b9815792a674f45281
Pedro Alonso Moreno Salcedo <pedroalonsoms@Pedros-MacBook-Air.local>
2023-08-04 13:43:19 -0700

    create node example for ES6 modules and require

---
+++ node-commonjs-example/a.js
@@ -0,0 +1,15 @@
+export function hello(name) {
+  console.log(`HELLO ${name}`);
+}
+
+export function bye(name) {
+  console.log(`BYE ${name}`);
+}
+
+function howareyou(name) {
+  console.log(`HOW ARE YOU ${name}`);
+}
+
+function test(name) {
+  console.log(`JUST TESTING ${name}`);
+}
---
+++ node-commonjs-example/b.js
@@ -0,0 +1,4 @@
+import { hello, bye } from "./a.js";
+
+hello("Pedro");
+bye("Pedro");
